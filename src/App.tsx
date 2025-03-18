084dfd1e96e8e79f3c4450af703342b212ae2ada
VITOR MARCELO BARZICK NOGUEIRA <vitor.nogueira@cs.up.edu.br>
2025-03-17 21:16:11 -0300

    react front

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,23 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import axios from "axios";
+import ListarProdutos from './components/ListarProdutos';
+import React, { useEffect } from "react";
 
 function App() {
+
+  useEffect(()=> {
+    axios.get("http://localhost:5256/api/produto/listar")
+      .then((response)=>{
+        console.log(response);
+      })
+
+  },[])
+
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Olá mundo em React</h1>
+      <ListarProdutos></ListarProdutos>
     </div>
   );
 }
-
 export default App;
---
+++ src/components/ListarProdutos.tsx
@@ -0,0 +1,11 @@
+function ListarProdutos(){
+    return(
+        <div>
+            <h1>Componente da Listagem de Produtos</h1>
+            <table></table>
+
+        </div>
+    );
+}
+
+export default ListarProdutos;
